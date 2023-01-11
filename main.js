5b98edf2c15b48b2015d2b806cc2fa4a10ea980b
Samiru73 <64921940+Samiru73@users.noreply.github.com>
2023-01-11 18:16:28 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+ScrollReveal().reveal('header');
+ScrollReveal().reveal('#Logo', { delay: 250 });
+ScrollReveal().reveal('#Dely', { delay: 500 });
+ScrollReveal().reveal('#Part2', { delay: 250 });
+ScrollReveal().reveal('#TXI', { delay: 500 });
+ScrollReveal().reveal('.Paf1', { delay: 250 });
+ScrollReveal().reveal('.Paf2', { delay: 250 });
+ScrollReveal().reveal('#FNL', { delay: 250 });
+ScrollReveal().reveal('#DX', { delay: 500 });
